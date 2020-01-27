bf0db8eee1ce3e36cc388b479318f1c3661f0772
marknesh <markmunene.nesh@gmail.com>
2020-01-27 10:49:00 +0300

    initialize the test using __init__ method

--- run.py
+++ run.py
@@ -44,17 +44,21 @@ def main():
     print("enter username")
 
     username = input()
+
+
+
     with open("username.txt",  "w") as userData:
         data = userData.write(username)
     save_username(create_username(username))
 
     print("enter password or type generate to generate a new password")
 
+
     password = input()
     save_password(create_password(password))
     with open("password.text", "w") as passwordData:
         datapassword = passwordData.write(password)
-        print("your password length is " + str(len(password)))
+
 
 
     print("type show to  display your credentials")
@@ -72,6 +76,7 @@ def main():
 
             for password in display_password():
                 print(f"password: ", {getpass(12)})
+                print("your password length is " + str(len(getpass(12))))
                 return
 
 
@@ -80,6 +85,7 @@ def main():
 
         for password in display_password():
             print(f"password: ", {password.password})
+            print("your password length is " + str(len(password.password)))
 
 
 
